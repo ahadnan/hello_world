b707bb621f86defeffcfd9dfc65426e506944447
mnzr <minhaz.ratul@gmail.com>
2014-11-08 13:53:07 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import printer
+
+print "Hello WOrld!"
+
+printer.pr("Ha Ha")
---
+++ printer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+"""
+The ret.py script shows how to work with
+functions in Python. 
+author: Jan Bodnar
+ZetCode, 2011
+"""
+
+
+def pr(i):
+    print i
